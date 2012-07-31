f48120a673e6438a349f565b41175e714fa7fe9b
Matthijs Drenth <matthijs.drenth@gmail.com>
2012-07-31 21:29:32 +0200

    App Store Changes
    Modified some code to make Plex appstore review possible

--- nosOS2012.bundle/Contents/Code/__init__.py
+++ nosOS2012.bundle/Contents/Code/__init__.py
@@ -1,9 +1,7 @@
-#nosOS2012.bundle VideoPlugin v1.0.0
+#nosOS2012.bundle VideoPlugin v1.0.1
 #Created by Matthijs Drenth
 
-import sys
 import urllib 
-import re
 
 ####################################################################################################
 
@@ -13,19 +11,18 @@ NAME = 'Olympische Spelen 2012'
 
 ART  = 'art-default.jpg'
 ICON = 'icon-default.png'
+BASE_URL = 'http://nos.nl/os2012'
 
 ####################################################################################################
 
 def Start():
 
     Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
-
     Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
     Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
     Plugin.AddViewGroup("PanelStream", viewMode="PanelStream", mediaType="items")
-    
-
-    MediaContainer.title1 = NAME
+	
+	MediaContainer.title1 = NAME
     MediaContainer.viewGroup = "InfoList"
     MediaContainer.art = R(ART)
     DirectoryItem.thumb = R(ICON)
@@ -37,19 +34,16 @@ def Start():
 ####################################################################################################
 
 def VideoMainMenu():
-
 	dir = MediaContainer(viewGroup="InfoList")
-	url ='http://nos.nl/os2012/wedstrijdschema/'
+	url = BASE_URL + '/wedstrijdschema/'
 	itemnr = 00
 	for item in HTML.ElementFromURL(url).xpath('//div[@class="channels"]/ol/li'):
 		itemnr = itemnr + 01
-
-		
 		if itemnr > 1:
 			channel = item.xpath('a/h3/em')[0].text
 			watch = item.xpath('p[1]')[0].text
 			next = item.xpath('//p[@class="later"]')[0].text
-			sport = item.xpath('a/h3')[0].get('data-sport-name')
+			sport = item.xpath('a/h3')[0].get('data-sport-name').capitalize()
 			until = item.xpath('a/div[@class="meta"]/span[@class="time"]')[0].text
 		else:
 			channel = '13'
@@ -61,31 +55,15 @@ def VideoMainMenu():
 			medal = ''
 		title = sport + " - kanaal " + channel
 		summary = "Nu: " + sport + " " + " "+ watch + " " + until + "\n\n" + next
-				
-		dir.Append(Function(VideoItem(GetUrl, title=title, summary=summary, subtitle=None, thumb=None), url=channel, title=title))
+		dir.Append(Function(VideoItem(GetUrl, title=title, summary=summary, subtitle=sport, thumb=None), view=channel))
 	return dir
 
 ####################################################################################################
 
-
-def GetUrl(sender, url, title):
-	url = 'plex://localhost/video/:/webkit?url=http://nos.nl/os2012/play/' + url +'/'
-	return Redirect(url)
-		
-####################################################################################################
-
-def GetThumb(vl):
-	sock = urllib.urlopen(vl) 
-	pagecontent = sock.read()                            
-	sock.close()
-	
-	m2 = re.search('mp4_file=(.{0,})(_ipad.mp4\")', pagecontent)
-	thumb = m2.group(1)
-	thumb = "http://media.autoweek.nl/m/" + thumb + "_m.jpg"
-	try:
-		image = HTTP.Request(thumb, cacheTime=CACHE_1MONTH).content
-		return DataObject(image, 'image/jpeg')
-	except:
-		return Redirect(R(PLUGIN_ICON_DEFAULT))
+def GetUrl(sender, view):
+	url = BASE_URL + '/play/' + view + '/'
+	Log('opening : ' + url)
+	### for v2 plugins ###
+	return Redirect(WebVideoItem(url))
 		
 ####################################################################################################
